52b56df60316647bf801f348fd2cc79d5dbc3587
JakubG-git <jakub.macius@gmail.com>
2019-10-06 15:01:33 +0200

    Update  do dokonczenia

--- PL/jedynki.cpp
+++ PL/jedynki.cpp
@@ -2,6 +2,14 @@
 #include<fstream>
 #include<windows.h>
 using namespace std;
+struct drzewo
+{
+    drzewo * lewo;
+    drzewo * prawo;
+    int poziom;
+    int dane;
+};
+
 int main()
 {
     system("chcp 1250");
@@ -10,15 +18,29 @@ int main()
     plik.open("JED.IN", ios::in);
     if(plik.good()==false)
     {
-        cout <<"Brak pliku";
+        cout <<"Brak pliku z danymi wejściowymi";
     }
     unsigned short int ilosc_lini;
-    plik >> ilosc_lini;    
-    unsigned int tablica[ilosc_lini];;
+    plik >> ilosc_lini;        
+    unsigned int tablica[15];    
     for (unsigned short i = 2; i <= ilosc_lini+1 ; i++)
     {
         plik >> tablica[i-2];
     } 
+    plik.close();
+    for (unsigned short int  i = 0; i < ilosc_lini; i++)
+    {
+        int liczba = 1;
+        while (liczba  % tablica[i] !=0)
+        {
+            //
+        }
+        if (liczba  % tablica[i] ==0)
+        {
+            cout << liczba << endl;
+        }      
+        
+    }
     
     
 }
