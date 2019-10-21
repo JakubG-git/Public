891aff06c26aabdf4e86244cc32d68860ec1c745
JakubG-git <jakub.macius@gmail.com>
2019-10-21 15:25:55 +0200

    backup

--- Cpp/hanoi.cpp
+++ Cpp/hanoi.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace  std;
-void hanoi(unsigned short int n, char a, char b,char c)
+void hanoi(unsigned short int n, char przenosze_Z, char bufor,char cel)
 {
-  if (n > 0)
-  {
-    hanoi(n-1, a, c, b);
-    cout << a << " -> " << c << endl;
-    hanoi(n-1, b, a, c);
-  }
+  if (n==0)
+  	return;
+  hanoi(n-1, przenosze_Z, cel, bufor);
+  cout << przenosze_Z << " -> " << cel << endl;
+  hanoi(n-1, bufor, przenosze_Z, cel);  
 }
-
 int main()
 {
     hanoi(2, '1', '2', '3');
---
+++ Cpp/pwn.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace  std;
+int main()
+{
+	cout <<  100  / 1 ;
+    cout <<  204  / 2 ;
+    cout <<  153  / 3 ;
+    cout <<  204  / 4 ;
+    cout <<  485  / 5 ;
+    cout <<  294  / 6 ;
+    cout <<  343  / 7 ;
+    cout <<  776  / 8 ;
+    cout <<  918  / 9 ;
+    cout <<  990  / 10;
+    cout <<  616  / 11;
+    cout <<  672  / 12;
+    cout <<  1326 / 13; 
+    cout <<  1386 / 14; 
+    cout <<  765  / 15;
+    cout <<  896  / 16;
+    cout <<  901  / 17;
+    cout <<  918  / 18;
+    cout <<  1083 / 19; 
+    cout <<  960  / 20;
+    cout <<  1197 / 21 ;
+    cout <<  1254 / 22 ;
+    cout <<  2254 / 23 ;
+    cout <<  1200 / 24 ;
+    cout <<  1375 / 25 ;
+    cout <<  2626 / 26 ;
+    cout <<  2673 / 27 ;
+    cout <<  1568 / 28 ;
+    cout <<  1653 / 29 ;
+    cout <<  3060 / 30 ;
+    cout <<  3162 / 31 ;
+    cout <<  1760 / 32 ;
+}
