87ed144fbc04ade91bd9d8f886734678a9532baf
JakubG-git <jakub.macius@gmail.com>
2019-11-03 20:50:08 +0100

    [Update] Added support for delete and put method [check description] - [Implemented] deletePostAlien method - [Implemented] updatePostAlien method

--- Java/boot_jpa/src/main/java/com/jakubG/demo/controller/AlienController.java
+++ Java/boot_jpa/src/main/java/com/jakubG/demo/controller/AlienController.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +33,19 @@ public class AlienController {
 	}
 	@PostMapping(path ="/alien")
 	public Alien addPostAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+	@DeleteMapping(path = "/aliens")
+	public String deletePostAlien(@RequestParam int aid)
+	{
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		return "deleted";
+	}
+	@PutMapping(path = "/alien")
+	public Alien updatePostAlien(@RequestBody Alien alien)
+	{
 		repo.save(alien);
 		return alien;
 	}
